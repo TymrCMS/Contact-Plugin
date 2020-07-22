0bcb3c75646b3537fcd275a15e91096f29725659
Sam McDonald <s.mcdonald@outlook.com.au>
2020-07-22 19:36:32 +1000

    final commit before upgrading Laravel

--- Storage/Routes/web.php
+++ Storage/Routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-
 Route::middleware(['web'])->namespace('Tymr\Plugins\Contact\Controllers')->group( function() {
 
-	
 	Route::get('/Contact', 'ContactController@index')->name('app.contact.show');
 
 	Route::post('/Contact',  'ContactController@sendContactRequest')->name('app.contact.post');
 
-
 });
-
